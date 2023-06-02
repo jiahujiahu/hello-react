777727346c9a66f4c680c0b1bc8e5792d2020e05
Jia Hu <jia.hu0918@gmail.com>
2023-06-02 12:28:16 -0400

    fetch todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function fetchTodos() {
+	return [
+		{ 
+			id: 1,
+			title: "吃饭",
+			completed: false,
+    },
+		{ 
+			id: 2,
+			title: "刷牙",
+			completed: false,
+		},
+		{ 
+			id: 3,
+			title: "喝水",
+			completed: false,
+		},
+		{ 
+			id: 4,
+			title: "洗澡",
+			completed: false,
+		},
+	];
 }
+
+function App() {
+	const todos = fetchTodos();
+
+	return (
+	  <>
+			<ul>
+				{todos.map((todo) => (
+						<li>
+							<input type="checkbox" defaultChecked={todo.completed} />
+							<label>{todo.title}</label>
+						</li>
+					))}
+			</ul>
+		</>
+		);
+	}
 
 export default App;
